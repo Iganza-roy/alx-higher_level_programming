4a3e521693483efc2fa399653fb84884c7d4db79
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-02 11:20:17 -0800

    nature of digit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, "is postitive")
+    print(number, "is positive")
 if number == 0:
     print(number, "is zero")
 if number < 0:
