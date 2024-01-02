8e6e6ea8cc387130293de11cf421f382199115b0
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-02 14:37:09 -0800

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for k in range(ord('a'), ord('z') + 1):
-        if c in chr(k):
-            return (True)
-    return (False)
+    if c >= 'a' and c <= 'z':
+        return (True)
+    else:
+        return (False)
