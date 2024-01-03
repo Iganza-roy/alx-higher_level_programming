7b7a8976999daa8b7bc7edf42a30e2f7acf6924e
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-03 11:45:56 -0800

    added tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') -1, -1):
-    print("{:c}{:c}".format(i, i - (ord('a') - ord('A'))), end="")
+for i in range(ord('z'), ord('a') -1, -1):
+    if i % 2 == 0:
+        res = 0
+    else:
+        res = 32
+    print("{}".format(chr(i - res)), end="")
