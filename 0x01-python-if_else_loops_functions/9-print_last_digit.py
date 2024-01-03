9dc50843b96fdf5ee8a7d66d9624bfd83039ab49
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-03 02:52:18 -0800

    modified

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return(number[-1])
+    if number > 0:
+        last = number % -(10)
+        print (-(last), end="")
+    else:
+        last = number % 10
+        print(last, end="")
+    return abs(last)
