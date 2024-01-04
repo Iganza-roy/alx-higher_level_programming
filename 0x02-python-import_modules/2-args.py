2717b60b463c223b2a6a25bb88b5db4aa28bd813
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-04 12:29:07 -0800

    added 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+argcnt = len(sys.argv) - 1
+if argcnt == 0:
+    print("0 arguments.")
+elif argcnt == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(argcnt))
+for i in argcnt:
+    print("{}. {}".format(i + 1, sys.arg[i + 1]))
