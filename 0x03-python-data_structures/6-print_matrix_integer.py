ce66252b71cb55cc2d2ce9aea8f23e4f39478ec1
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-06 05:50:24 -0800

    second

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for r in matrix:
-        res = ""
         for n in r:
-            res += "{:d}, ".format(n)
-        print(res[:-2])
+            print("{:d}".format(n), end="" if n == r[-1] else " ")
+        print()
