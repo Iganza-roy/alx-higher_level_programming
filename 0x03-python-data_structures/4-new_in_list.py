0ff8a55e592300cd46e87319f2f61cc0857e1cc5
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-06 01:34:20 -0800

    new modifications

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     new_list =my_list[:]
     if idx < 0:
-        return my_list
+        return new_list
     elif idx > len(my_list) - 1:
         return new_list
     else:
