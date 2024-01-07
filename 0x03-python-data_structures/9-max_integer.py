0467e24999273988b07a45bea2a09e86b01a201c
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-07 09:07:03 -0800

    added max_int

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,4 +7,4 @@ def max_integer(my_list=[]):
         for i in range(len(my_list)):
             if my_list[i] > greater:
                 greater = my_list[i]
-        print("{:d}".format(greater))
+        return greater
