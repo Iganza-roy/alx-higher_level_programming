74dd58056f465827513f44fe901ce10efc41bc54
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-07 08:48:14 -0800

    few mods

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        Return None
+        Return "None"
     else:
         greater = my_list[0]
         for i in my_list:
