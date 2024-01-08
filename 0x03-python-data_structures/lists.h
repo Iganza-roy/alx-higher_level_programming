18159830402207620d86bd034c84626321a0aef0
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-08 13:30:31 -0800

    header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -19,7 +19,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
+listint_t *rev_list(listint_t *head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
