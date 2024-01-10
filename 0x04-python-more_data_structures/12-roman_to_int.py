66ca43705becd09c187a9981c4fad3b764cea73e
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-10 12:12:37 -0800

    added roman_to_int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    num = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    total = 0
+    prev_val = 0
+
+    for char in reversed(roman_string):
+        value = num[char]
+        total += value if value >= prev_val else -value
+        prev_val = value
+    return total
+
+
