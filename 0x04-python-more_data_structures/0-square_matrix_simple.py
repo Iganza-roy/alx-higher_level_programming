7c63e6330c2c291f6fe747dc5940267904da3b8e
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-10 05:35:52 -0800

    alternative

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,3 +2,9 @@
 def square_matrix_simple(matrix=[]):
     new_mtx = [[i ** 2 for i in k] for k in matrix]
     return new_mtx
+
+
+"""
+or, using the map function:
+return [list(map(lambda x: x ** 2, row)) for row in matrix]
+"""
