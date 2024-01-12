4be64f8593306f9430b80dd136d65816d0b17876
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-12 12:05:16 -0800

    few mods

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,3 +2,12 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
+    add = 0
+    den = 0
+    for prod, weight in my_list:
+        add += prod * weight
+        den += weight
+    if den == 0:
+        return 0
+    avg = add / den
+    return avg
