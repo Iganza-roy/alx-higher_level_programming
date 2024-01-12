7bb9fe68d7583eb830ed12fe4c21b219e6ad445a
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-12 12:48:12 -0800

    added complex_delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    a_dictionary = {k: v for k, v in a_dictionary.items() if v != value}
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
     return a_dictionary
