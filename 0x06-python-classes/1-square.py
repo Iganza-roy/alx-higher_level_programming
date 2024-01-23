274cf49e8ec6734dc7b1f616c0cae3eb053fb6ae
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-23 08:42:08 -0800

    mods

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,9 +5,7 @@ class Square:
 
     def __init__(self, size):
         """initializing a new square class with an initial value
-        for attributes
         Args:
             size: the size of the square; which is very crucial in computation.
-            It is therefore set as a private attribute
         """
         self.__size = size
