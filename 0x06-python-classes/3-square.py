1e7162240ad6c0212f7d902c206f36ecd42a0582
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-23 09:45:24 -0800

    added 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""creating a class called square"""
+
+
+class Square:
+    """defining a square class"""
+
+    def __init__(self, size=0):
+        """initializing a new square class with an initial value
+        Args:
+            size: the size of the square; which is very crucial in computation.
+            size must be an integer and greater than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """public instance method that returns the current square area"""
+            return (self.__size * self.__size)
