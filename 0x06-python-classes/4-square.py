8c6586f51b5dadf7937d998091a1ee2bd2041c2b
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-24 10:23:00 -0800

    aded 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""creating a class called square"""
+
+
+class Square:
+    """defining a square class"""
+
+    def __init__(self, size=0):
+        """initializing a new square class with an initial value
+        Args:
+            size: the size of the square; which is very crucial in computation.
+            size must be an integer and greater than 0
+        """
+        self.size = size
+    @property
+    def size(self):
+        """set the current size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """public instance method that returns the current square area"""
+            return (self.__size ** 2)
