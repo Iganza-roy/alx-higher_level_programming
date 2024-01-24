b68dc4c47296da7a0767e2b1a45cadf7b14d4030
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-24 10:27:14 -0800

    indentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
-"""creating a class called square"""
+"""creating a class called square."""
 
 
 class Square:
-    """defining a square class"""
+    """defining a square class."""
 
     def __init__(self, size=0):
-        """initializing a new square class with an initial value
+        """initializing a new square class with an initial value.
         Args:
             size: the size of the square; which is very crucial in computation.
-            size must be an integer and greater than 0
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -18,6 +17,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """public instance method that returns the current square area"""
-            return (self.__size ** 2)
+    def area(self):
+        """public instance method that returns the current square area"""
+        return (self.__size ** 2)
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,6 +26,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """public instance method that returns the current square area"""
-            return (self.__size ** 2)
+    def area(self):
+        """public instance method that returns the current square area"""
+        return (self.__size ** 2)
