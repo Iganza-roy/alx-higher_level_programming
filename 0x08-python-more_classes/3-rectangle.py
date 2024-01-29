8e9c474696eff9b097aeef5906c6fe0b59952e25
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-29 11:16:09 -0800

    modified pycodestyle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -64,8 +64,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rect = [''.join(['#' for _ in range(self.__width)]) + '\n' 
+        rect = [''.join(['#' for _ in range(self.__width)]) + '\n'
                 for _ in range(self.__height - 1)]
         rect.append(''.join(['#' for _ in range(self.__width)]))
         return ''.join(rect)
-
