36a082a57f49bf8250bff3588617f5e6958c17a9
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-30 07:43:12 -0800

    added 2-matrix_divided.py and test files

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
+matrix = [[1, 2, "einirnf"],
+        [4, 5, 6]]
 print(matrix_divided(matrix, 3))
 print(matrix)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,10 +13,12 @@ def matrix_divided(matrix, div):
     div (int/float): the divisor
 
     """
-    if not all(isinstance(i, list) and
-            all(isinstance(j, (int, float)) for j in i) for i in matrix):
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(j, list) for j in matrix) or
+            not all((isinstance(i, int) or isinstance(i, float))
+                    for i in [n for j in matrix for n in j])):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if any(len(i) != len(matrix[0]) for i in matrix):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of a matrix must have a same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
