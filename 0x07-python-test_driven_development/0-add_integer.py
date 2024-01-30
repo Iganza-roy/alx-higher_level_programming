c6a486cf22b297d2f8858778b36039adf362eeaa
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-01-30 10:13:08 -0800

    added 5-text_indentation.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,7 +13,7 @@ def add_integer(a, b=98):
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance (b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     a = int(a) if isinstance(a, float) else a
