6dd79a8d0137d0a6103340c3bbd71d707b2f950e
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-05 10:34:36 -0800

    modified pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,6 +5,8 @@ is an instance of a class that inherited
 (directly or indirectly) from the specified
 class ; otherwise False.
 """
+
+
 def inherits_from(obj, a_class):
     """
     defining the function
