f3fe63e45468cdce55a3798ad446b77ff4400fee
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-05 10:50:20 -0800

    added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class basegeometry
+"""
+
+
+class BaseGeometry:
+    """
+    defining the class
+    """
+    def area(self):
+        raise Exception("area is not implemented")
