17517325cf03dc243f79847d682732091fd36705
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-05 08:52:50 -0800

    added 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+a function that returns True if the object is exactly an instance
+of the specified class ; otherwise False
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    defining the function
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
