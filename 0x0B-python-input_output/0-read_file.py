1042a41670f9908c03a0f602174df7fe0e229281
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-06 03:43:40 -0800

    docstyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+a function that reads a text file (UTF8) and prints it to stdout
+"""
+
 
 def read_file(filename=""):
+    """
+    Defining the function
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
