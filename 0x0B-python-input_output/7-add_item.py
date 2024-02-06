2dbca5c35b249f3734bb028eabf918e52fdf0674
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-02-06 05:05:08 -0800

    tested

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        items = load_from_json_file("add_item.json")
+        ls = load_from_json_file("add_item.json")
     except FileNotFoundError:
         ls = []
     ls.extend(sys.argv[1:])
