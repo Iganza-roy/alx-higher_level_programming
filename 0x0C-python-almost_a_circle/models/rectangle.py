79b44c6446c9a6beee754029e7df170b30f0ed1b
Iganza-roy <iganzaroy55@gmail.com>
2024-02-09 06:43:31 -0800

    added rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+class rectangle that inherits from base
+"""
+
+
+class Rectangle(Base):
+    """
+    defining the class rectangle
+    """
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+    def __init__(self, width, height, x=0, y=0, id=None):
+
