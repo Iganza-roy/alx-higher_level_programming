437b5ecff48daee9e59e9ae678e82103859b8b4a
Iganza-roy <iganzaroy55@gmail.com>
2024-02-09 06:00:21 -0800

    added models, base.py and 0-main.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+creating a class base
+"""
+
+
+class Base:
+    """
+    defining the class Base
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        initializing the class Base
+        Args:
+            id (int): argument
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
