4eb9564c64dcb1ce3c50fbae5508733d7af19c6c
Iganza-roy <iganzaroy55@gmail.com>
2024-02-10 09:09:22 -0800

    added rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,9 +8,19 @@ class Rectangle(Base):
     """
     defining the class rectangle
     """
-    __width -> width
-    __height -> height
-    __x -> x
-    __y -> y
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        initializing the Rectangle class
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectanle
+            x (int): x coordinate of rectangle
+            y (int): y coordinate of rectangle
+            id (int): identity of rectangle
+        """
 
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
