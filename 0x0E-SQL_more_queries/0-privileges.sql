b7532c4fdd9aeb0671fd97db4b20a78cd76cbd27
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-03-13 10:13:34 -0700

    modified 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
-SELECT Grant_priv, Super_priv, Create_tmp_table_priv, Lock_tables_priv, Create_view_priv, Show_view_priv, Create_routine_priv, Alter_routine_priv, Create_user_priv, Event_priv, Trigger_priv, Create_tablespace_priv
-FROM mysql.user
-WHERE User IN ('user_0d_1', 'user_0d_2')
-  AND Host = 'localhost';
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on the server.
 
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
