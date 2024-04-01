23eeb3498b3692ab2eee1d2edbf29f9946dbde85
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-01 12:52:38 -0700

    added doc strings

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+defining the roman_to_int function
+"""
+
+
 def roman_to_int(roman_string):
+    """
+    converts a Roman numeral to an integer.
+    """
     if not roman_string or type(roman_string) != str:
         return 0
     num = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
