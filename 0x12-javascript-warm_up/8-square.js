37bef102800c9989be23912e1a9a2455f17a3318
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-08 13:17:15 -0700

    modified coding standard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,16 +4,16 @@ const args = parseInt(process.argv[2]);
 let i = 0;
 
 if (isNaN(args)) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	while (i < args) {
-		let k = 0;
-		let row = '';
-		while (k < args ) {
-			row += 'X';
-			k++;
-		}
-		console.log(row);
-		i++;
-	}
+  while (i < args) {
+    let k = 0;
+    let row = '';
+    while (k < args) {
+      row += 'X';
+      k++;
+    }
+    console.log(row);
+    i++;
+  }
 }
