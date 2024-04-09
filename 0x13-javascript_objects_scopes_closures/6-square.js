c8c98acc311ba1b62a2b91f1e16afd342a3b5ded
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-09 15:12:24 -0700

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,15 @@
 
 const NewSquare = require('./5-square');
 
-class Square extends NewSquare{
-      charPrint(c) {
-         if (c === undefined) {
-            c = 'X';
-	 }
-         for (let i = 0; i < this.height; i++) {
-             console.log(c.repeat(this.width));
-	 }
-      }
+class Square extends NewSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Square;
