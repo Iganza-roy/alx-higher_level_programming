d3834741534fe5d7c6f7e9d06c1589e3be7571ba
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-09 13:27:39 -0700

    update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
+    if (w > 0 && h > 0) {
+	this.width = w;
+	this.height = h;
     } else {
-      this.width = w;
-      this.height = h;
+      return {};
     }
   }
 }
