974671f981e9300e8f29af33de6266386149afc8
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-12 06:14:10 -0700

    Add 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+script thattakes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+                host="localhost", port=3306, user=argv[1],
+                passwd=argv[2], db=argv[3]
+                )
+        cur = db.cursor()
+
+        state_name = argv[4]
+        query = """
+                SELECT *\
+                FROM states\
+                WHERE name = %s\
+                ORDER BY states.id ASC;\
+        """
+
+        cur.execute(query, (state_name,))
+
+        out = cur.fetchall()
+
+        for row in out:
+            print(row)
+
+        cur.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
