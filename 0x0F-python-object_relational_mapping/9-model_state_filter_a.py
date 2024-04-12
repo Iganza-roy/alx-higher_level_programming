1399c174dc9d83a0af56980a2d87b13ff3da3f72
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-12 12:53:03 -0700

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all State objects that contain 
+script that lists all State objects that contain
 the letter a from the database hbtn_0e_6_usa
 """
 import sys
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
