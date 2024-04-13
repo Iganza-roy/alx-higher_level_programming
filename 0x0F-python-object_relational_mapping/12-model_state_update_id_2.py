980dbeed6dd7c21158c642a8e55bc4605b38ae65
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-13 07:33:51 -0700

    Add 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+a script that prints the State object with
+the name passed as argument from the database hbtn_0
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+            )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    n = session.query(State).get(2)
+    if n:
+        n.name = "New Mexico"
+        session.commit()
