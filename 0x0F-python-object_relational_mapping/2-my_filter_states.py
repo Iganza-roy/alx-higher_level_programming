f1f8a0344ee95fed7c109c938b14dde129552b83
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-13 06:13:01 -0700

    DUpdate

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         query = """
                 SELECT *
                 FROM states
-                WHERE name = '{}'
+                WHERE name LIKE BINARY '{}'
                 ORDER BY states.id ASC;
         """.format(state_name)
 
