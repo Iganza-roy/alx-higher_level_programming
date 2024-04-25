e7fab8ba2429695353149427eda2a5545e52c6c0
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-25 13:13:11 -0700

    Update 0-bydy_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,5 @@
 #  Bash script that takes in a URL, sends a request to that URL
 # and displays the size of the body of the response
 
-curl -SI "$1" | awk '/contnt-Length/{print $2}'
+curl -sI "$1" | awk '/content-Length/{print $2}'
 
