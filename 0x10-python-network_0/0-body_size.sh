3f85ddf2882fb614482c28bf33a940d82faff5e1
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-25 13:09:04 -0700

    Add 0-body_size.sh

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#  Bash script that takes in a URL, sends a request to that URL
+# and displays the size of the body of the response
+
+curl -SI "$1" | awk '/contnt-Length/{print $2}'
+
