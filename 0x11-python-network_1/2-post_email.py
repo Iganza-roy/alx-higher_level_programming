63da4db4e02038c71a8c8a4d2a6ea28f523b216c
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-26 07:02:48 -0700

    Add 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter
+and displays the body of the response (decoded in utf-8)
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url, email = sys.argv[1], sys.argv[2].encode('utf-8')
+    req = urllib.request.Request(url, data=email, method='POST')
+    with urllib.request.urlopen(req) as response:
+        req_data = response.read().decode('utf-8')
+
+    print("Your email is:", req_data)
