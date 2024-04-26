c8962f0a496b5a975f53c54cbd64fe3901235db9
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-26 13:05:59 -0700

    Add 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,8 +8,8 @@ import sys
 
 if __name__ == "__main__":
     url, email = sys.argv[1], sys.argv[2]
-    #email_enc = email.encode('utf-8')
-    req = urllib.request.Request(url, data=email.encode('utf-8'), method='POST')
+    email_enc = email.encode('utf-8')
+    req = urllib.request.Request(url, data=email_enc, method='POST')
     with urllib.request.urlopen(req) as response:
         req_data = response.read().decode('utf-8')
     print(req_data)
