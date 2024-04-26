3ad29be39f524caadbb031ba06defb69c1e7c322
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-04-26 14:08:36 -0700

    Add 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    cred = (username, password)
+
+    r = requests.get("https://api.github.com/user", auth=cred)
+
+    if r.status_code == 200:
+        u_id = r.json().get("id")
+        print(u_id)
+
+    else:
+        pass
