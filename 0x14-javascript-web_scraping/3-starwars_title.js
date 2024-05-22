afe8dbb25f456c6f5ce13423ae0e9f7604dad957
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-22 10:56:13 -0700

    Add 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 
+const request = require('request');
+const epNum = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(url + epNum, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const responseJ = JSON.parse(body);
+    console.log(responseJ.title);
+  } else {
+    console.log('Error code:', response.statusCode);
+  }
+});
