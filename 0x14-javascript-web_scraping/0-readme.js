8f972a2832b136a4e22346d0ec0ebca301656b6f
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-22 06:48:31 -0700

    Update 0-readme.JS

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-let file_name = process.argv[2];
+const filename = process.argv[2];
 
-fs.readFile(file_name, 'utf8', (err, data) => {
-   if (err) {
-      console.error(err);
-      return;
-   }
-   console.log(data);
+fs.readFile(filename, 'utif8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -58,3 +58,4 @@ try {
 ```
 
 [![js-semistandard-style](https://raw.githubusercontent.com/standard/semistandard/master/badge.svg)](https://github.com/standard/semistandard)
+[![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg)](https://github.com/standard/semistandard)
