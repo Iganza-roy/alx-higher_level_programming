2489f7f42abfc92a45ae20b3e75f575b30befef1
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-22 10:06:37 -0700

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,6 @@ const request = require('request');
 const page = process.argv[2];
 
 request(page, function (error, response, body) {
+  console.error(error);
   console.log('code: ', response && response.statusCode);
 });
