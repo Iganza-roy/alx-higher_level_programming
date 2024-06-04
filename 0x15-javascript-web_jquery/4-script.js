8194f957deec867e348b35553c8dd9e018357284
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-06-04 18:41:30 +0300

    Update 4-script.js

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,11 +1,5 @@
 $(document).ready(function () {
 	$('#toggle_header').click(function() {
-		if ($('header').hasClass('red')) {
-			$('header').removeClass('red').addClass('green');
-		} else if ($('header').hasClass('green')) {
-			$('header').removeClass('green').addClass('red');
-		} else {
-			$('header').addClass('red');
-		}
+		$('header').toggleClass('red, green');
 	});
 });
